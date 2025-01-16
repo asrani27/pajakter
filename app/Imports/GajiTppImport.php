435dc5cdf9a627ecc43667756131026f24965042
asrani <asrani.27@gmail.com>
2025-01-16 10:16:04 +0800

    f

--- app/Imports/GajiTppImport.php
+++ app/Imports/GajiTppImport.php
@@ -14,30 +14,48 @@ class GajiTppImport implements ToModel, WithStartRow
      * @return \Illuminate\Database\Eloquent\Model|null
      */
     protected $bulan_tahun_id;
+    protected $existingNips;
 
     public function __construct($id)
     {
         $this->bulan_tahun_id = $id;
+
+        $this->existingNips = Pajak::where('bulan_tahun_id', $this->bulan_tahun_id)
+            ->pluck('nip', 'id')
+            ->toArray();
     }
 
     public function model(array $row)
     {
-        // Cek apakah data sudah ada berdasarkan 'bulan_tahun_id' dan 'nip'
-        $existingPajak = Pajak::where('bulan_tahun_id', $this->bulan_tahun_id)
-            ->where('nip', $row[0])
-            ->first();
+        $nip =  (string) trim($row[0]);
+        $pph_gaji = $row[11];
+        $gaji = $row[12];
+        // dd(in_array($nip, $this->existingNips), $this->existingNips);
+        // dd('d', $nip, $this->existingNips);
+        // dd($this->existingNips[$nip]);
+        // Cek jika NIP sudah ada di database
+        if (in_array($nip, $this->existingNips)) {
 
-        // Jika data sudah ada, tidak perlu menyimpan
-        if ($existingPajak) {
-            return null;
+            // Ambil ID pajak yang sudah ada
+            $pajakId = array_search($nip, $this->existingNips);
+            
+            // Update jumlah_tanggungan jika data sudah ada
+            Pajak::where('id', $pajakId)
+                ->update([
+                    'pph_gaji' => $pph_gaji,
+                    'gaji' => $gaji
+                ]);
+        } else {
+            return new Pajak([
+                'bulan_tahun_id' => $this->bulan_tahun_id,
+                'nip' => $row[0],
+                'nama' => $row[1],
+                'status_kawin' => $row[2],
+                'jumlah_tanggungan' => $row[4],
+                'pph_gaji' => $row[11],
+                'gaji' => $row[12],
+            ]);
         }
-        return new Pajak([
-            'bulan_tahun_id' => $this->bulan_tahun_id,
-            'nip' => $row[0],
-            'nama' => $row[1],
-            'status_kawin' => $row[2],
-            'jumlah_tanggungan' => $row[4],
-        ]);
     }
     public function startRow(): int
     {
